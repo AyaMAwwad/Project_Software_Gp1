4b2c13661d49d19091efc26cb5b072647b49cd30
AyaMAwwad <s12027735@stu.najah.edu>
2024-03-09 18:22:26 +0200

    aega2

--- project/lib/src/screen/new_pass.dart
+++ project/lib/src/screen/new_pass.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 import 'package:project/src/screen/login_screen.dart';
 import 'package:project/widgets/button_2.dart';
